25f652db94a4e6ae8b71d09cec6a6aca95a2b36c
Anonymous <>
2019-09-24 23:49:48 -0400

    Anonymous submission

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='advex-uar',
+        version='0.0.1.dev',
+        author='Anonymous Submission',
+        author_email='Anonymous Submission',
+        url='Anonymous Submission',
+        packages=['advex_uar'],
+        license='Apache',
+)
